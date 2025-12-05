7e462bc23051114791819c94b924af59d510961d
guyzmuch <guyzmuch@gmail.com>
2025-12-05 11:13:05 +0100

    git ssh key generation with better prompt, error catching and easier change of the algorithm

--- git/git_ssh_setup.sh
+++ git/git_ssh_setup.sh
@@ -4,16 +4,29 @@
 
 # Default values
 key_name="github"
+algorithm="ed25519" # you might also want to use "rsa"
+bits=2048 # default value for the number of bits (ignored for "ed25519" algorithm )
 file_path="$HOME/.ssh/$key_name"
 passphrase=""
 
 # Requesting email to link to the key
-echo "ENTER YOUR GITHUB ADDRESS:"
-read email
+read -p "ENTER YOUR GITHUB EMAIL ADDRESS: " email
 
-# Generating the key (passphrase is empty by defaut to prevent promt from ssh-keygen)
-echo "GENERATING THE KEY:"
-ssh-keygen -t ed25519 -C $email -N "$passphrase" -f $file_path
+# Generating the key (passphrase is empty by defaut to prevent prompt from ssh-keygen)
+echo "GENERATING THE KEY..."
+if [ "$algorithm" -eq  "ed25519" ]; then
+  ssh-keygen -t $algorithm -C $email -N "$passphrase" -f $file_path
+else
+  # for other algorithm like "rsa" you include the number of bits
+  ssh-keygen -t $algorithm -C $email -N "$passphrase" -b $bits -f $file_path
+fi
+key_generation_status=$?
+
+if [ $key_generation_status -ne 0 ]; then
+  echo "SSH KEY GENERATION FAILED"
+  exit 1
+fi
+
 
 # (if problem using ssh, add this script to the .bashrc file to launch it automatically at shell starup)
 # Start the ssh-agent
@@ -31,7 +44,7 @@ if [ -n "$passphrase" ]; then
   passphrase_text="WITH passphrase"
 fi
 echo ""
-echo "SSH key saved $passphrase_text in $file_path"
+echo "SSH KEY SAVED $passphrase_text IN $file_path"
 
 # Display information to paste into github
 echo "NOW, MANUALLY PASTE THE CONTENT OF $file_path.pub INTO GITHUB. FILE CONTENT:"
